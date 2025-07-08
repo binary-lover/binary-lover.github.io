358f9360b223435c00f0b7f2d505f63cd3dbeb96
binary-lover <whitedevil.hamd@gmail.com>
2025-07-08 16:39:45 +0530

    2nd commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/binary-lover.github.io",
+  base: "/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
